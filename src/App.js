026c135f19d55e13be35d49d7fbef0541e317504
Shaalan Marwan <salsanjary@gmail.com>
2021-09-27 19:09:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import React from "react";
+import Header from "./Header/Header";
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundColor: props.color,
+      }}
+      className="App"
+    >
+      <Header />
     </div>
   );
 }
+
+// export default App;
+
+// class App extends React.Component {
+
+//   constructor(props){
+// super(props);
+//     this.state = {};
+
+//   }
+
+//   render() {
+
+//     return (
+//       <div className="App">
+//         <h1>Hello World</h1>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
